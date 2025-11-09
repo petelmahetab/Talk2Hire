0020a63e43a0e742589e4103bd0b1035b849b19f
Mahetab Patel <patelmahetab9020@gmail.com>
2025-11-10 02:04:29 +0530

    ready and checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import ProblemPage from "./pages/ProblemPage";
 import ProblemsPage from "./pages/ProblemsPage";
 import SessionPage from "./pages/SessionPage";
 
+
+
 function App() {
   const { isSignedIn, isLoaded } = useUser();
 
@@ -23,6 +25,7 @@ function App() {
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
         <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
         <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
+       
       </Routes>
 
       <Toaster toastOptions={{ duration: 3000 }} />
