3862d845a2cd667a0dbbb50afe2a214e337d544e
Mahetab Patel <patelmahetab9020@gmail.com>
2025-12-09 21:48:31 +0530

    changes are done vite config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,32 @@
-// frontend/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 2000, // remove 500kb warnings
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          clerk: ["@clerk/clerk-react"],
+          stream: ["stream-chat-react"],
+          ui: ["react-hot-toast"],
+        },
+      },
+    },
+  },
+
   server: {
     port: 5173,
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:3000", 
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/api"), 
       },
     },
   },
